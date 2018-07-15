50744979654e35d6f5958054fac9ccdc69a8454a
msingh02 <Mikhil_Singh@comcast.com>
2018-07-15 13:49:26 -0400

    Adding sum and diff funcnality

--- mypgm.c
+++ mypgm.c
@@ -2,7 +2,15 @@
 
 int main()
 {
+	int a,b;
+
+	a=3;
+	b=3;
+
 	printf("HelloWorld.\n");
 	printf("Git Fundamentals.\n");
+
+	printf("sum = %d\n", (a+b));
+	printf("diff = %d\n", (a-b));
 	return 0;
 }

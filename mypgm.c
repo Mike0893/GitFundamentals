c452afb33ecc67fc8b9da1c1105e1cd653cd35ea
msingh02 <Mikhil_Singh@comcast.com>
2018-07-20 16:09:55 -0400

    Adding user i/p feature

--- mypgm.c
+++ mypgm.c
@@ -4,8 +4,12 @@ int main()
 {
 	int a,b;
 
-	a=3;
-	b=3;
+	//a=3;
+	//b=3;
+	printf("Enter value of a:");
+	scanf("%d", &a);
+	printf("Enter value of b:");
+	scanf("%d", &b);
 
 	printf("Git Fundamentals.\n");
 
